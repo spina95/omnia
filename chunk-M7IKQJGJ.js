c7609ed445819971e1a4bca4c087905bd096ef6e
spina95 <50515354+spina95@users.noreply.github.com>
2025-12-19 21:38:01 +0000

    Deploying to gh-pages from @ spina95/omnia@88ec185c73d4942434ac473b4a4e2dfb3c3b5f3b 🚀

--- chunk-M7IKQJGJ.js
+++
@@ -1,1 +0,0 @@
-import{G as t,ca as r}from"./chunk-XVI4XSAG.js";var i=class e{headerSignal=r({title:""});header=this.headerSignal.asReadonly();setHeader(a){this.headerSignal.set({title:a})}clearHeader(){this.headerSignal.set({title:""})}static \u0275fac=function(n){return new(n||e)};static \u0275prov=t({token:e,factory:e.\u0275fac,providedIn:"root"})};export{i as a};
