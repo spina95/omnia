63d2eb3b51330e4cb40563bb69f27631e513b74a
andreaspinazzola <andreaspinazzola3195@gmail.com>
2025-12-29 14:47:13 +0100

    fix

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, importProvidersFrom } from '@angular/core';
-import { provideRouter, withHashLocation } from '@angular/router';
+import { provideRouter } from '@angular/router';
 import * as PlotlyJS from 'plotly.js-dist-min';
 import { PlotlyModule } from 'angular-plotly.js';
 
@@ -8,7 +8,7 @@ import { routes } from './app.routes';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(), 
-    provideRouter(routes, withHashLocation()),
+    provideRouter(routes),
     importProvidersFrom(PlotlyModule.forRoot(PlotlyJS))
   ],
 };
