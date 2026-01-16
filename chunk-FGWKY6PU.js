e2272926571d93b6428cd3547f30f5ee3acdb2d5
spina95 <50515354+spina95@users.noreply.github.com>
2026-01-16 20:41:38 +0000

    Deploying to gh-pages from @ spina95/omnia@ae790996dd878c43b03bcd63ff82d8b1cb6893b8 🚀

---
+++ chunk-FGWKY6PU.js
@@ -0,0 +1,1 @@
+import{L as a,ha as n}from"./chunk-5B5UU655.js";var o=class e{actionsTemplate=n(null);actions=this.actionsTemplate.asReadonly();setActions(t){this.actionsTemplate.set(t)}clearActions(){this.actionsTemplate.set(null)}static \u0275fac=function(l){return new(l||e)};static \u0275prov=a({token:e,factory:e.\u0275fac,providedIn:"root"})};export{o as a};
